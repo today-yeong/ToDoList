69e8ab754745a00996097d3dc8e39ce961e7a116
tomorrowuse <sogum@tomorrowuseui-MacBookPro.local>
2023-11-13 20:31:40 +0900

    3rd commit

--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import*
 
 '''
 def index(request):
@@ -10,4 +11,16 @@ def index(request):
 
 def index(request):
     return render(request, "my_to_do_app/index.html")
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+
+    new_todo= Todo(content =user_input_str)
+    new_todo.save()
+
+    return HttpResponse("메모한 건 여기에 쌓여요 : " + user_input_str)
+
+
+
+
 
